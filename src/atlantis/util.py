b8d2d7433e44cd693abed0e14d84a7a83bb140d7
CooledCoffee <cooledcoffee@gmail.com>
2014-05-15 13:42:44 +0800

    util.init

--- src/atlantis/util.py
+++ src/atlantis/util.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
+from decorated.util import modutil
 import inflection
 
 def calc_name(cls):
     name = inflection.underscore(cls.__name__)
     ss = name.split('_')
     return '_'.join(ss[:-1])
+
+def init():
+    modutil.load_tree('devices')
+    modutil.load_tree('rules')
+    
