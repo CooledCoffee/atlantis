01af765e77f28dd85ed8c13e26a7876a47343e95
CooledCoffee <cooledcoffee@gmail.com>
2014-05-15 13:44:14 +0800

    exports in package

---
+++ src/atlantis/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from atlantis.device import Device, Sensor, controller
+from atlantis.rule import Problem, Solution
+from atlantis.util import init
+
+Device = Device
+Sensor = Sensor
+controller = controller
+
+Problem = Problem
+Solution = Solution
+
+init = init
