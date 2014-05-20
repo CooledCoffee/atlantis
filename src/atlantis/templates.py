7d9da03bcaa5560e37510949bec0dd503f01bc2e
CooledCoffee <cooledcoffee@gmail.com>
2014-05-20 11:33:41 +0800

    templates

---
+++ src/atlantis/templates.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from decorated import once
+from jinja2.environment import Environment
+from jinja2.loaders import FileSystemLoader
+import os
+
+env = None
+
+@once
+def init():
+    global env
+    dirname = os.path.join(os.path.dirname(__file__), 'templates')
+    loader = FileSystemLoader(dirname)
+    env = Environment(loader=loader, extensions=['jinja2.ext.with_'], trim_blocks=True)
+    
+def render(template_name, **kw):
+    template = env.get_template(template_name)
+    return template.render(**kw)
