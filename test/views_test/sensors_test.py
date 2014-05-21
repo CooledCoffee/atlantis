c3c95f6dcb3ae11abb531cb097c1b1e9c6d57b87
CooledCoffee <cooledcoffee@gmail.com>
2014-05-21 16:15:26 +0800

    sensors.set api

--- test/views_test/sensors_test.py
+++ test/views_test/sensors_test.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
+from atlantis.db import SensorModel
+from atlantis.device import Device, Sensor
 from atlantis.views import sensors
 from datetime import datetime
 from decorated.base.dict import Dict
-from testutil import TestCase
+from fixtures2 import DateTimeFixture
+from testutil import DbTestCase
+import json
 
-class GetTest(TestCase):
+class ThermometerDevice(Device):
+    temperature = Sensor()
+
+class GetTest(DbTestCase):
     def test(self):
-        sensor = Dict(value=25, time=datetime(2000, 1, 1))
-        self.patches.patch('atlantis.device.devices', {'thermometer': Dict(temperature=sensor)})
-        result = sensors.get('thermometer.temperature')
+        # set up
+        self.useFixture(DateTimeFixture('atlantis.device.datetime', datetime(2000, 1, 1)))
+        with self.mysql.dao.create_session() as session:
+            session.add(SensorModel(name='thermometer.temperature', value=json.dumps(25), time=datetime(2000, 1, 1)))
+            
+        # test
+        with self.mysql.dao.SessionContext():
+            result = sensors.get('thermometer.temperature')
         self.assertEqual(datetime(2000, 1, 1), result['time'])
         self.assertEqual(25, result['value'])
     
+class SetTest(DbTestCase):
+    def test(self):
+        # set up
+        self.useFixture(DateTimeFixture('atlantis.device.datetime', datetime(2000, 1, 1)))
+        
+        # test
+        with self.mysql.dao.SessionContext():
+            sensors.set('thermometer.temperature', 25)
+        with self.mysql.dao.create_session() as session:
+            model = session.get(SensorModel, 'thermometer.temperature')
+            self.assertEqual(25, json.loads(model.value))
+            self.assertEqual(datetime(2000, 1, 1), model.time)
+            
