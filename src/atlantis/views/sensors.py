630049182c46fc6cd73744187441535128321d97
CooledCoffee <cooledcoffee@gmail.com>
2014-06-03 11:31:57 +0800

    minor change

--- src/atlantis/views/sensors.py
+++ src/atlantis/views/sensors.py
@@ -15,7 +15,7 @@ def all():
 
 @api
 def get(name):
-    sensor = _get_sensor(name)
+    sensor = _find_sensor(name)
     model = ctx.session.get(SensorModel, name)
     return {
         'error_rate': model.error_rate,
@@ -27,16 +27,16 @@ def get(name):
     
 @api
 def update(name):
-    sensor = _get_sensor(name)
+    sensor = _find_sensor(name)
     sensor.update(force=True)
     return get(name)
 
 @api
 def set(name, value):
-    sensor = _get_sensor(name)
+    sensor = _find_sensor(name)
     sensor.value = value
 
-def _get_sensor(name):
+def _find_sensor(name):
     dname, sname = name.split('.')
     dev = device.devices[dname]
     return getattr(dev, sname)
