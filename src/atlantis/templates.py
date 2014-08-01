630fde1a9fc2f5547b53349a8484c4c2982d5482
CooledCoffee <cooledcoffee@gmail.com>
2014-08-01 16:28:40 +0800

    show enabled state on rules page

--- src/atlantis/templates.py
+++ src/atlantis/templates.py
@@ -2,6 +2,7 @@
 from atlantis.base import ExpiredError
 from jinja2.environment import Environment
 from jinja2.loaders import FileSystemLoader
+import doctest
 import os
 
 env = None
@@ -19,7 +20,23 @@ def init(dirname=None):
     dirnames.append(os.path.join(os.path.dirname(__file__), 'templates'))
     loader = FileSystemLoader(dirnames)
     env = AtlantisEnvironment(loader=loader, extensions=['jinja2.ext.with_'], trim_blocks=True)
+    env.filters['test'] = _test
     
 def render(template_name, **kw):
     template = env.get_template(template_name)
     return template.render(**kw)
+
+def _test(condition, true_value, false_value=''):
+    '''
+    >>> _test(True, '1', '2')
+    '1'
+    >>> _test(False, '1', '2')
+    '2'
+    >>> _test(True, '1')
+    '1'
+    '''
+    return true_value if condition else false_value
+
+if __name__ == '__main__':
+    doctest.testmod()
+    
