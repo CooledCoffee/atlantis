35271a233076a603a1204c1ceecb5cd337f81165
CooledCoffee <cooledcoffee@gmail.com>
2015-03-10 16:38:03 +0800

    refactor problem views

--- test/views_test/problems_test.py
+++ test/views_test/problems_test.py
@@ -6,15 +6,15 @@ from testutil import DbTestCase
 class EnableTest(DbTestCase):
     def test_enable(self):
         with self.mysql.dao.SessionContext():
-            problems.enable('TEMPERATURE_TOO_HIGH', True)
+            problems.enable('thermometer.too_high', True)
         with self.mysql.dao.create_session() as session:
-            model = session.get(ProblemModel, 'TEMPERATURE_TOO_HIGH')
-            self.assertFalse(model.disabled)
+            model = session.get(ProblemModel, 'thermometer.too_high')
+            self.assertTrue(model.enabled)
             
     def test_disable(self):
         with self.mysql.dao.SessionContext():
-            problems.enable('TEMPERATURE_TOO_HIGH', False)
+            problems.enable('thermometer.too_high', False)
         with self.mysql.dao.create_session() as session:
-            model = session.get(ProblemModel, 'TEMPERATURE_TOO_HIGH')
-            self.assertTrue(model.disabled)
+            model = session.get(ProblemModel, 'thermometer.too_high')
+            self.assertFalse(model.enabled)
             
