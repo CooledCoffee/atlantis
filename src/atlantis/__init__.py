497e97e5d3c08532f8ec8d9feb464c5cd0fa9d2e
CooledCoffee <cooledcoffee@gmail.com>
2015-03-13 14:47:21 +0800

    package exports

--- src/atlantis/__init__.py
+++ src/atlantis/__init__.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
-from atlantis.core.device import AbstractDevice
 from atlantis import util
+from atlantis.core.controller import Controller
+from atlantis.core.device import AbstractDevice
+from atlantis.core.problem import Problem
+from atlantis.core.sensor import Sensor
+from atlantis.core.solution import Solution, DisableByProblem
 
 AbstractDevice = AbstractDevice
+sensor = Sensor
+controller = Controller
+problem = Problem
+solution = Solution
+disable_by_problem = DisableByProblem
 
 init = util.init
